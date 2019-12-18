fd9f2b1ccdf114ee0ca70037b295fb9d8f9682ad
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2019-12-18 20:05:30 +0300

    commit1

---
+++ StokTakip/Controllers/KategoriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Models.Entity;
+namespace StokTakip.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var values = db.Kategoriler.ToList();
+            return View(values);
+        }
+    }
+}
